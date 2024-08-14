07541e18451128ecd78186934d4581f4573f110a
Nikolaykrasnov <crasnov.colya2000@yandex.ru>
2024-08-14 12:04:59 +0500

    add assert

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,7 +1,7 @@
 from capitalize import capitalize
 
-assert capitalize('hello') != 'Hello'
-assert capitalize('') != ''
-
+assert capitalize('hello') == 'Hello'
+assert capitalize('') == ''
+assert capitalize('hello') == 'hello'
 
 print('Все тесты пройдены!')
